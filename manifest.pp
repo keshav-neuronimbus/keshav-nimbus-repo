3e9e0667ef877e0f5e8e7f1d0dbb2cba86641e4f
root <root@localhost.localdomain>
2017-09-01 01:57:34 -0400

    Hello

--- manifest.pp
+++ manifest.pp
@@ -4,3 +4,18 @@ mode => '0777',
 replace => true,
 content => "black and white\n",
 }
+# just declaring some random variables
+$var_fruit = 'apple' #string
+$mynum = 925455 #number
+$array_one = [99,22,77] # array numbers
+$array_two = ['brown,black,red'] #array string
+$num1 = 88.53 # float
+$num2 = 0547 # octal
+$num3 = 0xFFAA # hex
+
+# sinlge quotes for independent strings
+# double quotes for interpolation
+# curly {} brackets for 
+
+$message = "i have an ${var_fruit},and numbers ${array_one}."
+ 
